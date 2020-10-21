7877007827351e85eddd642aa411e892127243e8
CuiYuXi <cuiyuxi@foxmail.com>
2020-10-21 23:52:10 +0800

    add KeysTests

---
+++ FreeRedis.Tests/RedisClientTests/KeysTests.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace FreeRedis.Tests.RedisClientTests
+{
+    public class KeysTests : TestBase
+    {
+        [Fact]
+        public void Del()
+        {
+        }
+        [Fact]
+        public void Dump()
+        {
+        }
+        [Fact]
+        public void Exists()
+        {
+        }
+        [Fact]
+        public void Expire()
+        {
+        }
+        [Fact]
+        public void ExpireAt()
+        {
+        }
+        [Fact]
+        public void Keys()
+        {
+        }
+        [Fact]
+        public void Migrate()
+        {
+        }
+    }
+}
